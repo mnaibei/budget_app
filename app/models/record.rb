76179fc41fb3068eaff640fe364c7e141b64f05b
mnaibei <muchajulius@gmail.com>
2023-08-05 19:47:31 +0300

    linter fix

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,5 @@
 class Record < ApplicationRecord
-belongs_to :author, class_name: 'User'
-has_and_belongs_to_many :groups
-has_many :group_records, dependent: :destroy
+  belongs_to :author, class_name: 'User'
+  has_and_belongs_to_many :groups
+  has_many :group_records, dependent: :destroy
 end
