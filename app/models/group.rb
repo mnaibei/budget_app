7d4f45893b0bef16c88c45d6f59f7a80cb8d446c
mnaibei <muchajulius@gmail.com>
2023-08-06 21:32:21 +0300

    Added validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,9 @@ class Group < ApplicationRecord
   has_many :groups_records
   has_and_belongs_to_many :records, join_table: 'groups_records'
 
+  validates :name, presence: true
+  validates :icon, presence: true
+
   def total_amount
     records.sum(:amount)
   end
